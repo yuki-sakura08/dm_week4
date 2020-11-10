3394d94ffa7bb7b66cb62d6e3f05180bc70cb468
yuki_sakura08 <e185763@ie.u-ryukyu.ac.jp>
2020-11-10 17:19:50 +0900

    add polynomial2_features

--- datasets.py
+++ datasets.py
@@ -4,3 +4,12 @@ def load_linear_example1():
     X = np.array([[1,4],[1,8],[1,13],[1,17]])
     Y = np.array([7,10,11,14])
     return X,Y
+
+def load_nonlinear_example1():
+    X = np.array([[1,0.0],[1,2.0],[1,3.9],[1,4.0]])
+    Y = np.array([4.0,0.0,3.0,2.0])
+    return X,Y
+
+def polynomial2_features(input):
+    poly2 = input[:,1:]**2
+    return np.c_[input,poly2]
--- main.py
+++ main.py
@@ -11,3 +11,9 @@ model.fit(X,Y)
 print(model.theta)
 print(model.predict(X))
 print(model.score(X,Y))
+
+import datasets
+X,Y = datasets.load_nonlinear_example1()
+ex_X = datasets.polynomial2_features(X)
+print(ex_X)
+print(Y)
